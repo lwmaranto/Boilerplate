3dd479d0bb12142d9f57511e5f6e7265d53240fa
Laura Maranto <lauramaranto18@gmail.com>
2020-11-20 10:28:44 -0800

    installs and webapack

--- app/main.js
+++ app/main.js
@@ -2,8 +2,8 @@ import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 
-import store from './store'
-import Routes from './components/Routes'
+//import store from './store'
+//import Routes from './components/Routes'
 
 render(
   <Provider store={store}>
